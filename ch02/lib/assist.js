fd258c76008f29a0b51b10e448d75e7ec492190f
AlvinZhang86 <zhsh411@126.com>
2016-07-05 00:38:08 +0800

    add files

--- ch02/lib/assist.js
+++ ch02/lib/assist.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(
     function () {
-        $("#reset").click(
+        $("#reset").click(  // reset点击后清空页面的效果
             function () {
                 $("*").removeAttr("style");
                 $("div[class=none]").css(
@@ -12,13 +12,13 @@ $(document).ready(
             }
         );
 
-        $("input[type=button]").click(function () {
+        $("input[type=button]").click(function () { // 当button被点击的时候  如果isreset是勾选状态，发生一次reset的点击事件
             if($("#isreset").is(":checked")) {
                 $("#reset").click();
             }
         });
 
-        function animateIt() {
+        function animateIt() {  // 实现span的动态效果
             $("#mover").slideToggle("slow",animateIt);
         }
         animateIt();
